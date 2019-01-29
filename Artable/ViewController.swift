83ceba66d30f0f056fb9a1e855c12cc432767f8b
Ayşegül Koçak <aysegulkock@gmail.com>
2019-01-29 17:26:52 +0300

    Initial Commit

---
+++ Artable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Artable
+//
+//  Created by Ayşegül Koçak on 29.01.2019.
+//  Copyright © 2019 Ayşegül Koçak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
